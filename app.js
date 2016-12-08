addb3f03ddcfd8e1ea433d5a84cbd1e5d9ba16ec
David Lee <david.lee@r3cev.com>
2016-12-08 13:41:52 +0000

    Changed URL to davidleeuk repo

--- app.js
+++ app.js
@@ -9,21 +9,21 @@ var router = express.Router();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// GitHub hits this endpoint when there is pull request activity on the 
+// GitHub hits this endpoint when there is pull request activity on the
 // joeldudley/test repo.
 router.post('/', function(req, res) {
     gitAlert = req.body;
     if (gitAlert.action == 'opened') {
         console.log('Pull request opened - title is: ' + gitAlert.pull_request.title);
     }
-    
+
     res.end();
 })
 
 // Returns the open pull requests for the joeldudley/test GitHub repo.
 router.get('/', function(_, res) {
     var options = {
-        url: 'https://api.github.com/repos/joeldudley/test/pulls?state=open',
+        url: 'https://api.github.com/repos/davidleeuk/test/pulls?state=open',
         headers: {
             'User-Agent': 'request'
         }
@@ -34,7 +34,7 @@ router.get('/', function(_, res) {
     });
 })
 
-// The API can also be used to close a pull request, using the following 
+// The API can also be used to close a pull request, using the following
 // endpoint:
 //     PATCH https://api.github.com/repos/joeldudley/test/pulls/:number
 
